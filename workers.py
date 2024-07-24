aae135717acf86780064d7b254f477795ca690b8
Zohida <zohishka04@gmail.com>
2024-07-24 01:08:06 -0700

    invigilator class added

--- workers.py
+++ workers.py
@@ -14,3 +14,17 @@ admin = Administration("staff","John Smith", "AA0000101",
               "johnsmith@gmail.com", "1995/02/12", "male", "English", "998901001010",
               "full-time", "john_smith", 20000, "academic adviser")
 admin.work()
+
+# 2. Invigilator
+class Invigilator(Staff):
+    def __init__(self, type, name, pass_id, email, bd, gender, nationality, phone, occupation, profile, salary, title):
+        super().__init__(type, name, pass_id, email, bd, gender, nationality, phone, occupation, profile, salary, title)
+
+
+    def work(self):
+        print("An invigilator supervises exams to ensure they are conducted fairly and according to rules.")
+
+invigilator = Invigilator("staff","John Smith", "AA0000101",
+              "johnsmith@gmail.com", "1995/02/12", "male", "English", "998901001010",
+              "full-time", "john_smith", 20000, "invigilator")
+invigilator.work()
