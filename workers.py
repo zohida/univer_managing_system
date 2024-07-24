6b6f37b3231884d3770aa78ec021e677a1065997
Zohida <zohishka04@gmail.com>
2024-07-24 01:33:49 -0700

    HR class added

--- workers.py
+++ workers.py
@@ -99,3 +99,17 @@ janitors = Janitors("staff","John Smith", "AA0000101",
               "full-time", "john_smith", 20000, "janitors")
 janitors.work()
 
+# 8. HR
+class HR(Staff):
+    def __init__(self, type, name, pass_id, email, bd, gender, nationality, phone, occupation, profile, salary, title):
+        super().__init__(type, name, pass_id, email, bd, gender, nationality, phone, occupation, profile, salary, title)
+
+
+    def work(self):
+        print("HR (Human Resources) manages employee relations, recruitment, and organizational policies.")
+
+hr = HR("staff","John Smith", "AA0000101",
+              "johnsmith@gmail.com", "1995/02/12", "male", "English", "998901001010",
+              "full-time", "john_smith", 20000, "hr")
+hr.work()
+
