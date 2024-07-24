64bbd22fdf61b0a796f0bd923135422f027a97e6
Zohida <zohishka04@gmail.com>
2024-07-24 01:27:06 -0700

    Security class added

--- workers.py
+++ workers.py
@@ -70,3 +70,17 @@ student_advisor = StudentAdvisor("staff","John Smith", "AA0000101",
               "johnsmith@gmail.com", "1995/02/12", "male", "English", "998901001010",
               "full-time", "john_smith", 20000, "SA")
 student_advisor.work()
+
+# 5. Security
+class Security(Staff):
+    def __init__(self, type, name, pass_id, email, bd, gender, nationality, phone, occupation, profile, salary, title):
+        super().__init__(type, name, pass_id, email, bd, gender, nationality, phone, occupation, profile, salary, title)
+
+
+    def work(self):
+        print("Security involves protecting people, property, and information from harm or unauthorized access.")
+
+security = Security("staff","John Smith", "AA0000101",
+              "johnsmith@gmail.com", "1995/02/12", "male", "English", "998901001010",
+              "full-time", "john_smith", 20000, "security")
+security.work()
